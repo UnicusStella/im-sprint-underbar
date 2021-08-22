9b5ac70bbca6db41187830dd8414847665d0a5a9
UnicusStella <unicusstella@gmail.com>
2021-08-22 21:21:08 +0900

    drop function Complete

--- src/01_bareMinimum.js
+++ src/01_bareMinimum.js
@@ -128,6 +128,14 @@ _.take = function (arr, n) {
 // n이 배열의 길이를 벗어날 경우, 빈 배열을 리턴합니다.
 _.drop = function (arr, n) {
   // TODO: 여기에 코드를 작성합니다.
+  let newArr = [];
+  if (n === undefined || n < 0) return (newArr = arr);
+  if (n >= arr.length) return newArr;
+
+  for (let i = n; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 };
 
 // _.last는 배열의 마지막 n개의 element를 담은 새로운 배열을 리턴합니다.
